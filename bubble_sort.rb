32ec6e0eb1a2364b1a9c99cb39fb00ab51a446c6
WILbeerT-B <wilbertbundalian@gmail.com>
2023-11-11 22:24:53 -0800

    Put the logic inside bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,26 @@
-num_array = [5, 1, 4, 2, 8]
-size = num_array.length
-j = 1
+def bubble_sort(num_array)
+  size = num_array.length
+  j = 1
 
-loop do
-  swapped = false
-  p "array values: #{num_array}"
-  p "PASS #{j}"
-  for i in 1..size-1
-    
-    if num_array[i-1] > num_array[i]
-      temp = num_array[i-1]
-      num_array[i-1] = num_array[i]
-      num_array[i] = temp
-      p "swap #{num_array[i-1]} & #{num_array[i]}"
-      swapped = true
-    else
-      p "NO SWAP"
+  loop do
+    swapped = false
+    p "array values: #{num_array}"
+    p "PASS #{j}"
+    for i in 1..size-1
+      
+      if num_array[i-1] > num_array[i]
+        temp = num_array[i-1]
+        num_array[i-1] = num_array[i]
+        num_array[i] = temp
+        p "swap #{num_array[i-1]} & #{num_array[i]}"
+        swapped = true
+      else
+        p "NO SWAP"
+      end
     end
+    break unless swapped
+    j += 1
   end
-  break unless swapped
-  j += 1
 end
+
+bubble_sort([4, 3, 78, 2, 0, 2])
