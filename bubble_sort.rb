0c46b43bb20500763d590c641a3aa1a57b2f7520
WILbeerT-B <wilbertbundalian@gmail.com>
2023-11-11 23:21:52 -0800

    Reformat code, add comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,26 +1,41 @@
 def bubble_sort(num_array)
   size = num_array.length
-  j = 1
 
+  #count the number of pass
+  count = 1
+
+  #iterate and test boolean swapped 
   loop do
+
+    #will determine if sort is done
     swapped = false
-    p "array values: #{num_array}"
-    p "PASS #{j}"
+
+    p "Array values: #{num_array}"
+    p "PASS #{count}"
+
+    #iterate through
     for i in 1..size-1
-      
+
+      #test if current num is greater than the num next to it 
       if num_array[i-1] > num_array[i]
+        #print which numbers to be swapped
+        p "Swap [#{num_array[i-1]} & #{num_array[i]}]"
+        #swap the numbers
         temp = num_array[i-1]
         num_array[i-1] = num_array[i]
         num_array[i] = temp
-        p "swap #{num_array[i-1]} & #{num_array[i]}"
+        #change to true if something changed
         swapped = true
       else
-        p "NO SWAP"
+        p "NO SWAP [#{num_array[i-1]} & #{num_array[i]}]"
       end
     end
+    count += 1
+
+    #break out of the loop if no more swap in a pass
     break unless swapped
-    j += 1
   end
+  p "Sorted array: #{num_array}"
 end
 
-bubble_sort([4, 3, 78, 2, 0, 2])
+bubble_sort([4, 3, 1, 1, 17, 83, 23, 0, 5, 100])
